10697fdc3dd4c39fc5aca0f4d5cf34edc1401819
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-02-09 00:33:37 -0500

    Final.

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
 
 const url = "http://api.github.com/users/ryanurbanski"
 
-// Method 1
-// fetch(url)
-//     .then(function(response) {      // callback function
-//         return response.json()
-//     })
-//     .then(function(data) {
-//         console.log(data)           // second callback function
-//         document.querySelector('#profile-container').innerHTML = `<h2>${data.name}</h2>`
-//     })
-
-// Method 2: Arrow Funcions
 fetch(url)
     .then((result) => result.json())
     .then((data) => {
-        document.querySelector('#profile-container').innerHTML = `<div class="header-container">
-            <img id="avatar-pic" src=${data.avatar_url} >
-        </div>`
-        
-        document.querySelector('.header-container').innerHTML += `<h2>${data.name}</h2>`
-        document.querySelector('#profile-container').innerHTML += ` <div class="info-container">
-            <p>Location: ${data.location}</p>
-            <p>
-                GitHub URL: <a href=${data.url}>${data.name}</a>
-            </p>
-            <p>GitHub username: ${data.login}</p>
 
-        </div>`
+        // Header Div ---------------------------------------------------------------
+        // Picture
+        document.querySelector('#profile-container').innerHTML = 
+            `<div class="header-container">
+                <img id="avatar-pic" src=${data.avatar_url} >
+            </div>`
+        
+        // Name
+        document.querySelector('.header-container').innerHTML += `<h2>${data.name}</h2>`
+        
+        // Information Div -----------------------------------------------------------
+        // Location, URL, Username
+        document.querySelector('#profile-container').innerHTML += ` 
+            <div class="info-container">
+                <p>Location: ${data.location}</p>
+                <p>GitHub URL: <a href=${data.url}>${data.name}</a></p>
+                <p>GitHub username: ${data.login}</p>
+            </div>`
         return data.repos_url
     })
+
+    // Repo Div
     .then((reposUrl) => fetch(reposUrl))
     .then((result) => result.json())
     .then((data) => {
@@ -45,24 +42,3 @@ fetch(url)
             `
         }
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Taken from youtube video... learn this function syntax!
-// const getProfileData = () => {
-//     fetch("https://api.github.com/users/ryanurbanski")
-//         .then((data) => data.json())
-//         // .then((data) => console.log(data))
-// }
